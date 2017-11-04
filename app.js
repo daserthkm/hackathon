2b51ecf18ae42486879d970d3975f9a61fa736de
Daniel Seifert <daniel.seifert@heinekingmedia.de>
2017-11-04 18:33:10 +0100

    Added allowed methods to cors header

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH");
     next();
 });
 
